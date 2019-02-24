5d2147542174f36195aed0ae694b5bf56386af7a
tan <haitanle88@gmail.com>
2019-02-23 21:04:52 -0500

    get params data for indiviual menu item and use link-to routing

--- udaciMeals/app/routes/item.js
+++ udaciMeals/app/routes/item.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
+import Ember from 'ember';
 
 export default Route.extend({
+	model(params) {
+		return Ember.$.get('/menu/'+params.item_name+'.json');
+	}
 });

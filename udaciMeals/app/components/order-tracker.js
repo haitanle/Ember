51852e6c0976fc941e2ea0f6b84dde6cfde39b91
tan <haitanle88@gmail.com>
2019-02-25 15:55:34 -0500

    handle click event action object for add/removing item and changing the setDay

--- udaciMeals/app/components/order-tracker.js
+++ udaciMeals/app/components/order-tracker.js
@@ -4,6 +4,9 @@ import { inject as service} from '@ember/service';
 export default Component.extend({
 	orderManager: service('order-manager'),
 	actions: {
+		setDayTo(day){
+			this.get('orderManager').setSelectedDayTo(day);
+		},
 		remove(day, menuCategory){
 			this.get('orderManager').removeMenuOption(day, menuCategory);
 		}
